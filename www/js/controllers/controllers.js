b776c3ece65e5923c15693a758634b4ba03d76d2
Alan Accurso <accurso.alan@gmail.com>
2014-03-04 17:51:33 -0300

    Adds menu button and refactors index.html

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -4,6 +4,14 @@ angular.module('CostSplitter.controllers', [])
 // A simple controller that fetches a list of data from a service
 .controller('SplitterIndexCtrl', function($scope, SplitterService) {
   $scope.splitters = SplitterService.query();
+  $scope.rightButtons = [
+    { 
+      type: 'ion-navicon-round button-menu',
+      tap: function(e) {
+      	$scope.sideMenuController.toggleRight();
+      }
+    }
+  ];
 })
 
 // A simple controller that shows a tapped item's data
