2b16ea1ee700cfa4121524232c26e3013c80c87c
Alan Accurso <accurso.alan@gmail.com>
2014-03-04 21:10:03 -0300

    Changes event palette and adds 'split costs' button

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -4,17 +4,25 @@ angular.module('CostSplitter.controllers', [])
 // A simple controller that fetches a list of data from a service
 .controller('SplitterIndexCtrl', function($scope, SplitterService) {
   $scope.splitters = SplitterService.query();
-  $scope.rightButtons = [
-    { 
-      type: 'ion-navicon-round button-menu',
+  $scope.rightButtons = [{ 
+      type: 'ion-navicon-round button-menu button-clear',
       tap: function(e) {
       	$scope.sideMenuController.toggleRight();
       }
-    }
-  ];
+  }];
+  $scope.addEvent = function () {
+  	console.info('Add event clicked.');
+  };
 })
 
 // A simple controller that shows a tapped item's data
 .controller('EventCtrl', function($scope, $stateParams, SplitterService) {
   $scope.event = SplitterService.getEvent($stateParams.splitterId, $stateParams.eventId);
+  $scope.rightButtons = [{ 
+      type: 'ion-ios7-compose button-menu button-clear',
+      tap: function(e) {
+      	// TODO: compose new cost
+      	$scope.sideMenuController.toggleRight();
+      }
+  }];
 });
