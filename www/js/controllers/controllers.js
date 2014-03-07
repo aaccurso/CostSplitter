a9a974a0e5497f2cce4dc305e50392d2f5ba040e
Alan Accurso <accurso.alan@gmail.com>
2014-03-07 00:00:13 -0300

    Refactors nav icons

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -5,7 +5,7 @@ angular.module('CostSplitter.controllers', [])
 .controller('SplitterIndexCtrl', function($scope, SplitterService) {
   $scope.splitters = SplitterService.query();
   $scope.rightButtons = [{ 
-      type: 'ion-navicon-round button-menu button-clear',
+      type: 'ion-navicon-round button-icon button-clear',
       tap: function(e) {
       	$scope.sideMenuController.toggleRight();
       }
@@ -19,14 +19,14 @@ angular.module('CostSplitter.controllers', [])
 .controller('EventCtrl', function($scope, $stateParams, SplitterService) {
   $scope.event = SplitterService.getEvent($stateParams.splitterId, $stateParams.eventId);
   $scope.rightButtons = [{ 
-      type: 'ion-ios7-compose button-menu button-clear',
+      type: 'ion-compose button-icon button-clear',
       tap: function(e) {
       	// TODO: compose new cost
       	$scope.sideMenuController.toggleRight();
       }
     },
     {
-    	type: 'ion-trash-a button-menu button-clear',
+    	type: 'ion-trash-a button-icon button-clear',
     	tap: function(e) {
     		// TODO: delete event
     		// SplitterService.deleteEvent($stateParams.splitterId, $stateParams.eventId);
