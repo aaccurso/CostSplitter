341bf2eeba4f21be1edf60c97e858c02930b1abf
Alan Accurso <accurso.alan@gmail.com>
2014-03-12 23:58:51 -0300

    Integrates with sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,44 @@
 // Gruntfile.json
 
 module.exports = function(grunt) {
-
+  
+  // Reads package.json and loads grunt tasks.
+  require('load-grunt-tasks')(grunt);
+  
+  // Times how long tasks take.
+  require('time-grunt')(grunt);
+  
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    karma: {
-      // grunt karma
-      unit: {
-        configFile: 'karma.conf.js'
+    ionic: {
+      app: 'www'
+    },
+    watch: {
+      gruntfile: {
+          files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= ionic.app %>/templates/{,*/}*.html',
+          '<%= ionic.app %>/css/{,*/}*.css',
+          '<%= ionic.app %>/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      },
+      compass: {
+        files: ['<%= ionic.app %>/css/{,*/}*.{scss,sass}'],
+        tasks: ['compass', 'autoprefixer']
+      },
+      karma: {
+        // grunt karma
+        unit: {
+          configFile: 'karma.conf.js'
+        }
       }
-    }
+    },
     // Grunt Karma config example
     //karma: {
     //  options: {
@@ -30,8 +58,6 @@ module.exports = function(grunt) {
     //}
   
   });
-  
-  grunt.loadNpmTasks('grunt-karma');
 
 };
 
