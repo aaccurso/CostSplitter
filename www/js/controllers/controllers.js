99ba9dfb806ae00b1371447d32f3d2377cbb53fa
Alan Accurso <accurso.alan@gmail.com>
2014-03-16 22:35:48 -0300

    Changes ionic dependency to stable release

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -5,21 +5,24 @@ angular.module('CostSplitter.controllers', [])
 .controller('SplitterIndexCtrl', function($scope, SplitterService) {
   $scope.splitters = SplitterService.query();
   $scope.newEvent = {};
-  $scope.rightButtons = [{ 
-      type: 'ion-navicon-round button-icon button-clear',
+  $scope.rightNavButtons = [{ 
+      type: 'ion-navicon button-icon button-clear',
       tap: function(e) {
       	$scope.sideMenuController.toggleRight();
       }
   }];
-  $scope.addEvent = function (splitterId) {
-  	SplitterService.addEventToSplitter(splitterId, $scope.newEvent);
+  $scope.addEvent = function ($index) {
+  	SplitterService.addEventToSplitter(
+      $scope.splitters[$index].id,
+      $scope.newEvent
+    );
   };
 })
 
 // A simple controller that shows a tapped item's data
 .controller('EventCtrl', function($scope, $stateParams, SplitterService) {
   $scope.event = SplitterService.getEvent($stateParams.splitterId, $stateParams.eventId);
-  $scope.rightButtons = [{ 
+  $scope.rightNavButtons = [{ 
       type: 'ion-compose button-icon button-clear',
       tap: function(e) {
       	// TODO: compose new cost
