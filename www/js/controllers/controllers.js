5a2b03c365be0a65806d428dc4e43e856ea222b6
Alan Accurso <accurso.alan@gmail.com>
2014-03-17 00:47:38 -0300

    Adds cordova dependency

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,7 +1,7 @@
+'use strict';
+
 angular.module('CostSplitter.controllers', [])
 
-
-// A simple controller that fetches a list of data from a service
 .controller('SplitterIndexCtrl', function($scope, SplitterService) {
   $scope.splitters = SplitterService.query();
   $scope.newEvent = {};
@@ -19,7 +19,6 @@ angular.module('CostSplitter.controllers', [])
   };
 })
 
-// A simple controller that shows a tapped item's data
 .controller('EventCtrl', function($scope, $stateParams, SplitterService) {
   $scope.event = SplitterService.getEvent($stateParams.splitterId, $stateParams.eventId);
   $scope.rightNavButtons = [{ 
