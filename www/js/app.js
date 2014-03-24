31bc16c57da100446bb272c7aafc2d498708a265
Alan Accurso <accurso.alan@gmail.com>
2014-03-24 17:32:30 -0300

    Configures javascript facebook sdk for local testing

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,51 @@
 angular.module('CostSplitter', ['ionic', 'CostSplitter.services', 'CostSplitter.controllers', 'ezfb'])
 
 .constant('FB_CONFIG', {
-  FB_APP_ID: '473499536106818',
-  LOCALE: 'es_AR'
+  FB_APP_ID: '662664923796340',
+  LOCALE: 'es_AR',
+  PERMISSIONS: 'basic_info email publish_actions'.split(' ')
 })
 
 .config(function($stateProvider, $urlRouterProvider, $FBProvider, FB_CONFIG) {
 
+  var _fb_init_config = {};
+
   $FBProvider.setLocale(FB_CONFIG.LOCALE);
-  $FBProvider.setInitParams({
-    appId: FB_CONFIG.FB_APP_ID
-  });  
+
+  if ( typeof cordova === 'undefined' || typeof Cordova === 'undefined' ) { // Executing on mobile device
+
+  } else {
+    var _defaultLoadSDKFunction = [
+         '$window', '$document', '$fbAsyncInit', '$fbLocale',
+    function ($window,   $document,   $fbAsyncInit,   $fbLocale) {
+      // Load the SDK's source Asynchronously
+      (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/" + $fbLocale + "/all.js";
+        // js.src = "//connect.facebook.net/" + $fbLocale + "/all/debug.js";  // debug
+        ref.parentNode.insertBefore(js, ref);
+      }($document[0]));
+
+      $window.fbAsyncInit = $fbAsyncInit;
+    }];
+
+    $FBProvider.setLoadSDKFunction( _defaultLoadSDKFunction );
+
+    _fb_init_config = {
+      status: true,
+      cookie: true,
+      xfbml: true,
+      frictionlessRequests: true,
+      useCachedDialogs: true,
+      oauth: true
+    };
+
+  };
+
+  _fb_init_config.appId = FB_CONFIG.FB_APP_ID;
+  $FBProvider.setInitParams( _fb_init_config );
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
